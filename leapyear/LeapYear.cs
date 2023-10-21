4a811e20754ea3da20044d51640116913688e479
AKSHAY TANPURE <116140237+AkshayTanpure2210@users.noreply.github.com>
2023-10-21 23:28:27 +0530

    Update LeapYear.cs

--- leapyear/LeapYear.cs
+++ leapyear/LeapYear.cs
@@ -1,69 +1,69 @@
-namespace Transflower.learningApp{
+// namespace Transflower.learningApp{
 
-    public class LeapYear{
-        public  int x = 20;
-        public int y = 30;
-        public int z = 50;
-
-
-        public void LeapYearChecker1()
-        {
-            int year = 2020;
-
-            if (year % 400 == 0)
-                Console.WriteLine(year + " is a Leap Year");
-            else if (year % 4 == 0 && year % 100 != 0)
-                Console.WriteLine(year + " is a Leap Year");
-            else
-                Console.WriteLine(year + " is not a Leap Year");
-        }
-
-          public void LeapYearChecker2()
-         {
-            int year = 2020;
-
-            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
-                Console.WriteLine(year + " is a Leap Year");
-            else
-                Console.WriteLine(year + " is not a Leap Year");
-        }
+  //  public class LeapYear{
+     //   public  int x = 20;
+       // public int y = 30;
+        //public int z = 50;
 
 
+      //  public void LeapYearChecker1()
+      //  {
+      //      int year = 2020;
 
+         //   if (year % 400 == 0)
+            //    Console.WriteLine(year + " is a Leap Year");
+        //    else if (year % 4 == 0 && year % 100 != 0)
+          //      Console.WriteLine(year + " is a Leap Year");
+          //  else
+             //   Console.WriteLine(year + " is not a Leap Year");
+     //   }
 
-  public void LeapYearChecker3()
-         {
-            int year = 2019;
+        //  public void LeapYearChecker2()
+       //  {
+          //  int year = 2020;
 
-            int flag = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) ? 1 : 0;
-            if (flag == 1)
-                Console.WriteLine(year + " is a Leap Year");
-            else
-                Console.WriteLine(year + " is not a Leap Year");
-        }
+          //  if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+             //   Console.WriteLine(year + " is a Leap Year");
+          //  else
+               // Console.WriteLine(year + " is not a Leap Year");
+     //   }
 
 
 
-public void LeapYearChecker4(){
+
+ // public void LeapYearChecker3()
+       //  {
+           // int year = 2019;
+
+           // int flag = (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0) ? 1 : 0;
+          //  if (flag == 1)
+              //  Console.WriteLine(year + " is a Leap Year");
+           // else
+               // Console.WriteLine(year + " is not a Leap Year");
+   //     }
 
 
-            int year = 2020;
-            bool leap;
 
-            if (year % 400 == 0)
-                leap = true;
-            else if (year % 4 == 0 && year % 100 != 0)
-                leap = true;
-            else
-                leap = false;
+// public void LeapYearChecker4(){
 
-            if (leap)
-                Console.WriteLine(year + " is a leap year.");
-            else
-                Console.WriteLine(year + " is not a leap year.");
 
-}
+            // int year = 2020;
+        //    bool leap;
 
- }
+         //   if (year % 400 == 0)
+             //   leap = true;
+           // else if (year % 4 == 0 && year % 100 != 0)
+               // leap = true;
+         //   else
+       //         leap = false;
+//
+   //         if (leap)
+   //             Console.WriteLine(year + " is a leap year.");
+    //        else
+       //         Console.WriteLine(year + " is not a leap year.");
 
-}
+//}
+
+// }
+
+//}
