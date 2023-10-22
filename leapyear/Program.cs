8116116bce950b651474383dc6e80cf67361e160
AKSHAY TANPURE <116140237+AkshayTanpure2210@users.noreply.github.com>
2023-10-22 23:42:36 +0530

    Update Program.cs

--- leapyear/Program.cs
+++ leapyear/Program.cs
@@ -1,7 +1,7 @@
-﻿using Transflower.learningApp;
+//﻿using Transflower.learningApp;
 
-LeapYear lp = new LeapYear();
-lp.LeapYearChecker1();
-lp.LeapYearChecker2();
-lp.LeapYearChecker3();
-lp.LeapYearChecker4();
+//LeapYear lp = new LeapYear();
+//lp.LeapYearChecker1();
+//lp.LeapYearChecker2();
+//lp.LeapYearChecker3();
+//lp.LeapYearChecker4();
