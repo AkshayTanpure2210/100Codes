65ad7814514744a0ee207f09b9269b154d377024
AKSHAY TANPURE <116140237+AkshayTanpure2210@users.noreply.github.com>
2023-12-14 00:01:43 +0530

    Update PrimeNumber.cs

--- mathApp/PrimeNumber.cs
+++ mathApp/PrimeNumber.cs
@@ -1,57 +1,57 @@
-using System;
-using System.Security.Cryptography.X509Certificates;
+// using System;
+// using System.Security.Cryptography.X509Certificates;
 
-namespace TransflowerLearning.MathSolution
-{
-    public class PrimeNumber
-    {
-            int n = 23;
+// namespace TransflowerLearning.MathSolution
+// {
+//     public class PrimeNumber
+//     {
+//             int n = 23;
 
 
-        public void CheckPrimeNumber(int n)
-        {
+//         public void CheckPrimeNumber(int n)
+//         {
     
-            if (n < 2)
-            {
-                Console.WriteLine($"The given number {n} is not prime");
-                return;
-            }
+//             if (n < 2)
+//             {
+//                 Console.WriteLine($"The given number {n} is not prime");
+//                 return;
+//             }
 
             
-            if (n == 2)
-            {
-                Console.WriteLine($"The given number {n} is prime");
-                return;
-            }
+//             if (n == 2)
+//             {
+//                 Console.WriteLine($"The given number {n} is prime");
+//                 return;
+//             }
 
            
-            if (n % 2 == 0)
-            {
-                Console.WriteLine($"The given number {n} is not prime");
-                return;
-            }
+//             if (n % 2 == 0)
+//             {
+//                 Console.WriteLine($"The given number {n} is not prime");
+//                 return;
+//             }
 
-            // check for factors up to the square root of n
-            for (int i = 3; i <= Math.Sqrt(n); i += 2)
-            {
-                if (n % i == 0)
-                {
-                    Console.WriteLine($"The given number {n} is not prime");
-                    return;
-                }
-            }
+//             // check for factors up to the square root of n
+//             for (int i = 3; i <= Math.Sqrt(n); i += 2)
+//             {
+//                 if (n % i == 0)
+//                 {
+//                     Console.WriteLine($"The given number {n} is not prime");
+//                     return;
+//                 }
+//             }
 
-            Console.WriteLine($"The given number {n} is prime");
+//             Console.WriteLine($"The given number {n} is prime");
 
 
-    }
+//     }
 
-public  int Sum(){
-   int y=20;
-   int z =50;
-   int x =y+z;
-   Console.WriteLine(x);
-   return x;
-}
-}
-}
+// public  int Sum(){
+//    int y=20;
+//    int z =50;
+//    int x =y+z;
+//    Console.WriteLine(x);
+//    return x;
+// }
+// }
+// }
